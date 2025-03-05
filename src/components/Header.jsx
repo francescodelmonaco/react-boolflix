7be238997fb129d35ea5dd1b87677a6f573f5909
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-03-05 11:45:57 +0100

    add: global context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ export function Header() {
     return (
         <header>
             <h1>BoolFlix</h1>
+            <input type="text" placeholder="Cerca il nome del film" />
+            <button>Cerca</button>
         </header>
     )
 }
---
+++ src/contexts/GlobalContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_BASE_API_URL;
+
+// creazione provider per api calls
+const GlobalContext = createContext();
+const GlobalProvider = ({ children }) => {
+    // lista film
+    const [filmsList, setFilmsList] = useState({});
+
+    const getFilms = () => {
+        axios.get(apiUrl)
+            .then((res) => setFilmsList(res.data))
+    };
+
+    // lista serie tv
+    const [seriesList, setSeriesList] = useState({});
+
+    const getSeries = () => {
+        axios.get(apiUrl)
+            .then((res) => setSeriesList(res.data))
+    };
+
+    // destructuring
+    const value = {
+        filmsList,
+        seriesList,
+        getFilms,
+        getSeries
+    }
+
+    return (
+        <GlobalContext.Provider value={value}>
+            {children}
+        </GlobalContext.Provider>
+    )
+};
+
+const useGlobalContext = () => useContext(GlobalContext);
+
+export { GlobalProvider, useGlobalContext };
